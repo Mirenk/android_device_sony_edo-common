16e44cdfdd93ab024de64fb1e6d7576f4e727a27
Mirenk <minikmwinsea@gmail.com>
2021-02-16 20:10:51 +0900

    edo-common: pe->lineage

--- edo.mk
+++ edo.mk
@@ -22,7 +22,7 @@ PRODUCT_USE_PRODUCT_VNDK_OVERRIDE := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
